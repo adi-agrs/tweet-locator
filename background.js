c39a4d2edae38958e54da7613b23f2066f5796d4
Adi Agrawal <adityaagrawal9835@gmail.com>
2026-07-30 23:47:00 -0700

    make manifest.json

---
+++ background.js
@@ -0,0 +1,8 @@
+// listens for the extension being clicked
+// and opens "history.html" in a new tab
+
+chrome.action.onClicked.addListener(function() {
+    chrome.tabs.create({
+        url: chrome.runtime.getURL("history.html")
+    });
+});
